9378031aa70e12bcaedd4e04f14b56728ba579df
prateek951 <prince.cenation@gmail.com>
2018-04-03 00:22:38 +0530

    function importing the dataset

--- decision_tree.py
+++ decision_tree.py
@@ -6,3 +6,14 @@ from sklearn.cross_validation import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+
+# Function to import the dataset
+def import_dataset():
+    dataset = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-'+
+'databases/balance-scale/balance-scale.data',sep=',',header=None)
+    # Print the shape for the dataset
+    print(dataset.shape)
+    # Print the length for the dataset
+    print(len(dataset))
+    print(dataset.head())
+    return dataset
